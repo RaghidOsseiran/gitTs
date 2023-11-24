687b19cffe7cb28e85eae9765d68736d3ab45717
raghid osseiran <Raghidcrazy4@hotmail.com>
2023-11-25 00:38:18 +0100

    plusieur example du narrowing du type de retour

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,7 @@ const d: null = null
 const arr: string[] = ['aze', 'aze', 'aze']
 const user: {firstname: string, lastname?: string} = {firstname: "john", lastname: "Doe"}
 const userInf : {firstname: string, [key: string]: string} = {firstname: "John", lastname: "Doe"}
-const date: Date = new Date();
+const date: Date = new Date(); // On cree un object
 const cb: (e: MouseEvent) => void  = (e: MouseEvent): void => {
         // syntax pour une fonction qui prend en parametre un MouseEvent et return void
 }
@@ -21,17 +21,51 @@ function printId(id: number | string): void {
 
 
 */
-var compteur = document.querySelector('#compteur');
+var compteur = document.querySelector('#compteur'); // ou ! qui est du narrowing forcer
 var i = 0;
 var increment = function (e) {
     e.preventDefault();
     i++;
     var span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
-    if (span) {
+    if (span) { // narrowing, vue qu'on on entre dans cette condition donc forcement on elimine les possibilites que span soit null ou undefined
         span.innerText = i.toString();
     }
 };
 compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
+function printId(id) {
+    if (typeof id === "number") {
+        console.log((id * 3).toString());
+    }
+    else {
+        console.log(id.toUpperCase());
+    }
+}
+var date = new Date();
+function example(a) {
+    if (a instanceof Date) {
+        a;
+    }
+}
+function example2(a) {
+    if (Array.isArray(a)) {
+        return a[0];
+    }
+    return;
+}
+function example3(a) {
+    if ("value" in a) {
+        a; // detect que c'est du type HTMLInputElement car c'est le seul des deux type qui a une cle value
+    }
+}
+function isDate(a) {
+    return a instanceof Date;
+}
+function example4(a) {
+    if (isDate(a)) {
+        a;
+    }
+}
+// des typeof, des instaceof, egalite, Array.isArray ...
 // possibiliter que compteur soit null, solution 1:
 // if (compteur){
 //     compteur.addEventListener('click', increment)
--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const d: null = null
 const arr: string[] = ['aze', 'aze', 'aze']
 const user: {firstname: string, lastname?: string} = {firstname: "john", lastname: "Doe"}
 const userInf : {firstname: string, [key: string]: string} = {firstname: "John", lastname: "Doe"}
-const date: Date = new Date();
+const date: Date = new Date(); // On cree un object
 const cb: (e: MouseEvent) => void  = (e: MouseEvent): void => {
         // syntax pour une fonction qui prend en parametre un MouseEvent et return void
 }
@@ -24,20 +24,70 @@ function printId(id: number | string): void {
 */
 
 
-const compteur = document.querySelector('#compteur') as HTMLButtonElement
+const compteur = document.querySelector('#compteur') as HTMLInputElement // ou ! qui est du narrowing forcer
 let i = 0;
 
 const increment = (e: Event) => {
     e.preventDefault();
     i++
     const span = compteur?.querySelector('span')
-    if (span){
+    if (span){ // narrowing, vue qu'on on entre dans cette condition donc forcement on elimine les possibilites que span soit null ou undefined
         span.innerText = i.toString()
     }
 }
 
 
+
 compteur?.addEventListener('click', increment)
+
+
+
+function printId (id: string | number){
+    if (typeof id === "number"){
+        console.log((id*3).toString())
+    } else {
+        console.log(id.toUpperCase())
+    }
+}
+
+const date: Date = new Date();
+
+
+function example (a: string | Date){
+    if(a instanceof Date){
+        a
+    }
+}
+
+function example2(a: string | string[]){
+    if (Array.isArray(a)){
+        return a[0]
+    }
+    return
+}
+
+function example3(a: MouseEvent | HTMLInputElement){
+    if("value" in a){
+        a // detect que c'est du type HTMLInputElement car c'est le seul des deux type qui a une cle value
+    }
+
+}
+
+function isDate(a: any): a is Date {
+    return a instanceof Date
+}
+
+function example4(a: Date | HTMLInputElement){
+    if (isDate(a)){
+        a
+    }
+}
+
+
+// des typeof, des instaceof, egalite, Array.isArray ...
+
+
+
 
 
 // possibiliter que compteur soit null, solution 1:
